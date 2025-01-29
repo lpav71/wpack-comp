31b575ec533620fe9cdf1cba67c846c89e08ab48
Павел <lpav71@gmail.com>
2025-01-29 19:03:48 +0300

    Два Vue приложения

--- app.js
+++ app.js
@@ -4,20 +4,16 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "/css/styles.css";
 
-
 import { createApp } from 'vue';
 
 const app1 = createApp({});
-
 import HelloComponent from "./vueComponents/Hello.vue";
 app1.component('hello', HelloComponent);
 import AboutComponent from "./vueComponents/About.vue";
 app1.component('about', AboutComponent);
 
 app1.mount('#app1');
-
 const app2 = createApp({});
 import Test from "./vueComponents/TestComposition.vue";
 app2.component('test', Test);
-
 app2.mount('#app2');
