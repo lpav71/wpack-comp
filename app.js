4ce3969521ca5da93b10bda80f9cbea6782aba79
Павел <lpav71@gmail.com>
2025-01-29 19:07:38 +0300

    Два Vue приложения

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ import "/css/styles.css";
 import { createApp } from 'vue';
 
 const app1 = createApp({});
-import HelloComponent from "./vueComponents/Hello.vue";
+import HelloComponent from "./src/Hello.vue";
 app1.component('hello', HelloComponent);
-import AboutComponent from "./vueComponents/About.vue";
+import AboutComponent from "./src/About.vue";
 app1.component('about', AboutComponent);
 app1.mount('#app1');
 
 const app2 = createApp({});
-import Test from "./vueComponents/TestComposition.vue";
+import Test from "./src/TestComposition.vue";
 app2.component('test', Test);
 app2.mount('#app2');
